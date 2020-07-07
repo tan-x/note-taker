b8401d3d57c2ebad3f7fa9ae62dc2981657f3a17
Tanner Griffin <tannermgriffin@gmail.com>
2020-07-07 18:59:01 -0500

    updated require routes on server.js, corrected casing

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ app.use( express.static( __dirname + '/public' ));
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./routes/apiroutes")(app);
+require("./routes/htmlroutes")(app);
 
 // =============================================================================
 // LISTENER
